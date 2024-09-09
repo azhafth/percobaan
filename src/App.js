f9fa203b1bffdf069e35b9ef60838fcbb43e5641
azhafth <azhariafatiha10@gmail.com>
2024-09-09 16:56:45 +0700

    Deskripsi perubahan baru

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Person from './components/Person';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './components/appStyles.css'
-import styles from './components/appStyles.module.css'
+// import Form from './components/Form';
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+// import Hello from './components/Hello'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+// import ClassClick from './components/ClassClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import Person from './components/Person';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
+// import './components/appStyles.css'
+// import styles from './components/appStyles.module.css'
+
 
 class App extends Component {
   render() {
-  return (
+  return (  
     <div className="App">
-      <h1 className='error'>Error</h1>
-      <h1 className={styles.success}>Success</h1>
+      {/* <Form/> */}
+      {/* <h1 className='error'>Error</h1> */}
+      {/* <h1 className={styles.success}>Success</h1> */}
       {/* <Inline/> */}
       {/* <Stylesheet primary={true}/> */}
       {/* <Person/> */}
