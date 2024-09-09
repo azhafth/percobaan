f2358e507240495e38d4c960adf93f5f388ee836
azhafth <azhariafatiha10@gmail.com>
2024-09-09 08:48:39 +0700

    parentComponent

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class ParentComponent extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            parentName: 'Parent'
+        }
+    }
+
+    grettParent(){
+        alert('Hello ${this.state.parentName}')
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ParentComponent
